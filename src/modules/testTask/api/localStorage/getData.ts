f92737f00f7c14dc86cd2017dbab96cd53dfdabd
BaurinAnton <baurinanton2013@yandex.ru>
2025-04-21 10:51:02 +0300

    fix: type

--- src/modules/testTask/api/localStorage/getData.ts
+++ src/modules/testTask/api/localStorage/getData.ts
@@ -1,38 +1,39 @@
-import type { TLocalStorageDTO } from './DTO'
 import { CALCULATOR_AND_COUNTER } from './keys'
 
-export function getCalculatorAndCounterData(): TLocalStorageDTO | null {
+export function getCalculatorAndCounterData(): string | null {
   const jsonData = localStorage.getItem(CALCULATOR_AND_COUNTER)
   if (jsonData === null) return null
-  const parsedData = JSON.parse(jsonData)
-  const validatedData = validateData(parsedData)
 
-  return validatedData
+  return jsonData
+  // const parsedData = JSON.parse(jsonData)
+  // const validatedData = validateData(parsedData)
+
+  // return validatedData
 }
 
-function validateData(data: unknown): TLocalStorageDTO | null {
-  if (isLocalStorageDTO(data)) {
-    return data
-  }
+// function validateData(data: unknown): TLocalStorageDataDTO | null {
+//   if (isValidData(data)) {
+//     return data
+//   }
 
-  return null
-}
+//   return null
+// }
 
-function isLocalStorageDTO(data: unknown): data is TLocalStorageDTO {
-  return (
-    typeof data === 'object' &&
-    data !== null &&
-    'counter' in data &&
-    'price' in data &&
-    'quantity' in data &&
-    'amount' in data &&
-    isNumber(data.counter) &&
-    isNumber(data.price) &&
-    isNumber(data.quantity) &&
-    isNumber(data.amount)
-  )
-}
+// function isValidData(data: unknown): data is TLocalStorageDataDTO {
+//   return (
+//     typeof data === 'object' &&
+//     data !== null &&
+//     'counter' in data &&
+//     'price' in data &&
+//     'quantity' in data &&
+//     'amount' in data &&
+//     isNumber(data.counter) &&
+//     isNumber(data.price) &&
+//     isNumber(data.quantity) &&
+//     isNumber(data.amount)
+//   )
+// }
 
-function isNumber(x: unknown): x is number {
-  return typeof x === 'number'
-}
+// function isNumber(x: unknown): x is number {
+//   return typeof x === 'number'
+// }
